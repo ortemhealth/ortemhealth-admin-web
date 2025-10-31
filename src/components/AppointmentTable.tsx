c78ba0da6a5405bef300215cbf21042a9adf7ce4
Praveen Jha <95619152+praveen1743@users.noreply.github.com>
2025-10-31 18:12:18 +0530

    Update AppointmentTable.tsx

--- src/components/AppointmentTable.tsx
+++ src/components/AppointmentTable.tsx
@@ -1,1 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material';
+import { getAppointments } from '../services/appointment';
+
+export default function AppointmentTable() {
+  const [appointments, setAppointments] = useState([]);
+  useEffect(() => {
+    getAppointments().then(setAppointments);
+  }, []);
+  return (
+    <Paper>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Patient</TableCell>
+            <TableCell>Doctor</TableCell>
+            <TableCell>Time</TableCell>
+            <TableCell>Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {appointments.map(appt => (
+            <TableRow key={appt.id}>
+              <TableCell>{appt.patientName}</TableCell>
+              <TableCell>{appt.doctorName}</TableCell>
+              <TableCell>{appt.time}</TableCell>
+              <TableCell>{appt.status}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
 
