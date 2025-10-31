9833003941d1aa3bb3bfdeff7f9c417e8f57b079
Praveen Jha <95619152+praveen1743@users.noreply.github.com>
2025-10-31 20:06:07 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Avatar, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Avatar, Box, Select, MenuItem } from '@mui/material';
+import { useLocation } from '../contexts/LocationContext';
+
+const LOCATIONS = ["Main Clinic", "Branch 1", "Branch 2"];
 
 export default function Navbar() {
+  const { location, setLocation } = useLocation();
+
   return (
     <AppBar position="static" elevation={0}>
       <Toolbar>
         <Box sx={{ flexGrow: 1 }}>
-          <Typography variant="h6" color="inherit">
-            OrtemHealth Admin
-          </Typography>
+          <Typography variant="h6" color="inherit">OrtemHealth Admin</Typography>
+        </Box>
+        <Box sx={{ mr: 2 }}>
+          <Select
+            value={location}
+            onChange={e => setLocation(e.target.value)}
+            size="small"
+            displayEmpty
+            sx={{ bgcolor: 'white', minWidth: 140 }}
+            inputProps={{ 'aria-label': 'Select clinic location' }}
+          >
+            <MenuItem value=""><em>Select Location</em></MenuItem>
+            {LOCATIONS.map(loc => <MenuItem key={loc} value={loc}>{loc}</MenuItem>)}
+          </Select>
         </Box>
         <Avatar src="/assets/doctor-avatar.png" sx={{ ml: 2 }} />
       </Toolbar>
     </AppBar>
   );
 }
-
