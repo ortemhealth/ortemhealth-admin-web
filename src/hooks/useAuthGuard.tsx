f807c26da4582d40075ae8c9197c12bcb34beb95
Praveen Jha <95619152+praveen1743@users.noreply.github.com>
2025-10-31 20:01:05 +0530

    Update useAuthGuard.tsx

--- src/hooks/useAuthGuard.tsx
+++ src/hooks/useAuthGuard.tsx
@@ -1,13 +1,13 @@
-// src/hooks/useAuthGuard.tsx
 import { useRouter } from 'next/router';
 import { useAuth } from '../contexts/AuthContext';
 import { useEffect } from 'react';
 
-export function useAuthGuard() {
+export function useAuthGuard(requiredRole?: string) {
   const { user } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
     if (!user) router.replace('/login');
+    if (requiredRole && user?.role !== requiredRole) router.replace('/not-authorized');
   }, [user]);
 }
