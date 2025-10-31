4ebc9dd06618f54c7caabd7f03d83f0c111538ac
Praveen Jha <95619152+praveen1743@users.noreply.github.com>
2025-10-31 18:17:32 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Avatar, Box } from '@mui/material';
+
+export default function Navbar() {
+  return (
+    <AppBar position="static" elevation={0}>
+      <Toolbar>
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" color="inherit">
+            OrtemHealth Admin
+          </Typography>
+        </Box>
+        <Avatar src="/assets/doctor-avatar.png" sx={{ ml: 2 }} />
+      </Toolbar>
+    </AppBar>
+  );
+}
 
