5fcf77b5ae9b8ce021ed1209c0b3ac11ff383274
Praveen Jha <95619152+praveen1743@users.noreply.github.com>
2025-10-31 18:21:29 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,11 @@
+import { ThemeProvider } from '@mui/material/styles';
+import materialTheme from '../theme/materialTheme';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={materialTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
 
