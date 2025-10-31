791ccbcb0a0605d12091901fbd6c1d2ca1759b44
Praveen Jha <95619152+praveen1743@users.noreply.github.com>
2025-10-31 19:59:32 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-export const api = axios.create({
-  baseURL: 'https://api.ortemhealth.com', // Update for your backend
-  headers: { 'Content-Type': 'application/json' },
-});
 
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE,
+  headers: { 'Content-Type': 'application/json' }
+});
