94c611c99a3d34c21b514f19ad74e291d5e51b61
Praveen Jha <95619152+praveen1743@users.noreply.github.com>
2025-10-31 18:21:11 +0530

    Update materialTheme.ts

--- src/theme/materialTheme.ts
+++ src/theme/materialTheme.ts
@@ -1,1 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const materialTheme = createTheme({
+  palette: {
+    primary: { main: '#23289D' },
+    secondary: { main: '#00A896' },
+    background: { default: '#F7F7FA' },
+  },
+  typography: {
+    fontFamily: ['Montserrat', 'Arial', 'sans-serif'].join(','),
+  },
+});
+
+export default materialTheme;
 
